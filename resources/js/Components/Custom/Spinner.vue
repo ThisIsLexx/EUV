8a59e9387b1bf1956a63e6c1c1cbe5bd7de7fed1
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-05-23 02:07:21 -0600

    UI: Componente loader para cargas

---
+++ resources/js/Components/Custom/Spinner.vue
@@ -0,0 +1,21 @@
+<!-- Spinner.vue -->
+<template>
+    <div class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  border-left-color: #09f;
+  animation: spin 1s ease infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
