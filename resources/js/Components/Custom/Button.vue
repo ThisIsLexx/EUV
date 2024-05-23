6562428a2fe19427ced147f65d15e285b7535e20
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-05-23 03:08:16 -0600

    Build: Componentes para botón y ruta de navegación en listados

---
+++ resources/js/Components/Custom/Button.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Props {
+    message: string;
+    button: "button" | "submit" | "reset";
+    type: string;
+    disabled: boolean;
+    icon: string;
+}
+
+const props = defineProps<Props>();
+
+let bgColor = '';
+
+switch (props.type) {
+    case 'primary':
+        bgColor = 'bg-indigo-500 hover:bg-indigo-500/90';
+        break;
+    case 'secondary':
+        bgColor = 'bg-gray-500 hover:bg-gray-500/90';
+        break;
+    case 'danger':
+        bgColor = 'bg-red-500 hover:bg-red-500/90';
+        break;
+    default:
+        bgColor = 'bg-indigo-500 hover:bg-indigo-500/90';
+        break;
+}
+
+</script>
+
+<template>
+    <button :type="`${button}`" :class="`text-white p-2 rounded-md shadow-sm hover:shadow-md btn btn-${type} ${bgColor}`" :disabled="disabled">
+        <i v-if="props.icon" :class="`${props.icon}`"></i>
+        <span>{{ message }}</span>
+    </button>
+</template>
---
+++ resources/js/Components/Custom/Show.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { router } from '@inertiajs/vue3';
+
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline';
+
+interface Props {
+    id: string | number;
+    route: string;
+}
+
+const props = defineProps<Props>();
+
+function redirect() {
+    router.get(props.route);
+}
+
+</script>
+
+<template>
+    <div @click="redirect" class="flex space-x-2 max-w-10 content-center items-center underline text-sky-600 hover:text-sky-600/80 hover:cursor-pointer">
+        <span>
+            <ArrowTopRightOnSquareIcon class="h-4 w-4" />
+        </span>
+        <span class="underline text-sky-600 hover:text-sky-600/80">
+            {{ props.id }}
+        </span>
+    </div>
+</template>
