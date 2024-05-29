570f15818d3fabd9122b247ba7eee08447ad0f53
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-05-29 12:30:00 -0600

    UI: Cambios visuales

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                monserrat: ['Montserrat', 'sans-serif'],
             },
         },
     },
