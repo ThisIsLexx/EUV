d6a301f88d8b50b4b01c580c2d8006071a7e7340
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-08-13 01:06:08 -0600

    UI: Agregado transitionGroup para animar los elementos de los breadcrumbs

--- resources/js/Components/Custom/Breadcrumbs.vue
+++ resources/js/Components/Custom/Breadcrumbs.vue
@@ -13,16 +13,18 @@
                     </button>
                 </div>
             </li>
-            <li v-for="page in pages" :key="page.name">
-                <div class="flex items-center">
-                    <ChevronRightIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                    <button type="button" @click="redirectToPage(page.href)"
-                        :class="page.current ? 'font-semibold text-indigo-500 hover:text-indigo-600' : 'font-normal'"
-                        class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                        :aria-current="page.current ? 'page' : undefined">{{ page.name }}
-                    </button>
-                </div>
-            </li>
+            <TransitionGroup appear name="right-to-left">
+                <li v-for="(page, index) in pages" :key="page.name" :style="{ transitionDelay:  `${index * 200}ms` }">
+                    <div class="flex items-center">
+                        <ChevronRightIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                        <button type="button" @click="redirectToPage(page.href)"
+                            :class="page.current ? 'font-semibold text-indigo-500 hover:text-indigo-600' : 'font-normal'"
+                            class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                            :aria-current="page.current ? 'page' : undefined">{{ page.name }}
+                        </button>
+                    </div>
+                </li>
+            </TransitionGroup>
         </ol>
     </nav>
 </template>
