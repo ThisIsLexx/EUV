7dacbae3a956190e2ed89295d448c50c2e979a17
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-08-22 22:39:05 -0600

    wip: Cambios a inicio de sesión

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <slot name="logo" />
-        </div>
+    <div class="flex">
+        <!-- BEGIN: Card inicio de sesión -->
+        <div class="flex flex-col sm:justify-center w-1/3 items-center pt-6 sm:pt-0 px-20 z-10 bg-gray-100 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+            <div>
+                <slot name="logo" />
+            </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <slot />
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-lg overflow-hidden sm:rounded-lg">
+                <slot />
+            </div>
         </div>
+        <!-- END: Card inicio de sesión -->
+
+        <!-- BEGIN: Imagen de bienvenida -->
+        <div class="flex flex-col">
+            <img src="images/login.jpg" alt="Imagen de bienvenida" class="object-cover backdrop-blur-lg w-full h-screen" />
+        </div>
+        <!-- END: Imagen de bienvenida -->
     </div>
 </template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -43,7 +43,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" value="Correo electrónico" :required="true" />
                 <TextInput
                     id="email"
                     v-model="form.email"
@@ -57,7 +57,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Contraseña" />
                 <TextInput
                     id="password"
                     v-model="form.password"
@@ -72,17 +72,17 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ms-2 text-sm text-gray-600">Recuerdame</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Forgot your password?
+                    Olvidaste tu contraseña?
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
+                    Iniciar Sesión
                 </PrimaryButton>
             </div>
         </form>
