8b2a6f8a6e487e9ae57f06bed118060613dd0a0c
Higashi <seigi.higashi@gmail.com>
2024-09-22 20:46:45 -0600

    Cambios iniciales a pagina de inicio de usuario
    Modifique el Template .vue que corresponde a la pagina de bienvenida del usuario. La idea es ver si el acomodo esta bien y que mas cosas deberían de aparecer en dicha pagina.

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -1,90 +1,89 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import TextInput from './TextInput.vue';
+import { AcademicCapIcon, PencilIcon, RocketLaunchIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
     <div>
         <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+
             <ApplicationLogo class="block h-12 w-auto" />
 
             <h1 class="mt-8 text-2xl font-medium text-gray-900">
-                Welcome to your Jetstream application!
+                Bienvenido a Erase una vez
             </h1>
+            
 
-            <p class="mt-6 text-gray-500 leading-relaxed">
-                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
-            </p>
+
         </div>
-
+        <!-- Espacio para listado de actividades(?)-->
+        <!-- colores temporales-->
         <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+            
             <div>
-                <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-                    </svg>
-                    <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                        <a href="https://laravel.com/docs">Documentation</a>
-                    </h2>
-                </div>
-
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
-                </p>
-
-                <p class="mt-4 text-sm">
-                    <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
-                        Explore the documentation
-
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500">
-                            <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                </p>
+                <h1>seccion nueva??</h1>
+                <h6>quiza una imagen como el ejemplo</h6>
             </div>
 
             <div>
-                <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                        <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-                    </svg>
-                    <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                        <a href="https://laracasts.com">Laracasts</a>
-                    </h2>
+                <div>
+                    <div class="flex items-center">
+                        <AcademicCapIcon class="stroke-cyan-500 h-24 w-24"></AcademicCapIcon>
+                        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                            <a href="https://laravel.com/docs">Cursos</a>
+                        </h2>
+                    </div>
+
+                    <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse vel cumque ab doloremque recusandae illo nam enim impedit minus reiciendis ratione voluptatibus aspernatur odit, expedita consectetur eveniet! Assumenda, quae asperiores!
+                    </p>
+
+                    <p class="mt-4 text-sm">
+                        <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
+                            ENLACE????
+                        </a>
+                    </p>
                 </div>
 
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                </p>
+                <div>
+                    <div class="flex items-center">
+                        <PencilIcon class="stroke-cyan-100 h-12 w-12"></PencilIcon>
+                        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                            <a href="https://laracasts.com">Mecanografía</a>
+                        </h2>
+                    </div>
 
-                <p class="mt-4 text-sm">
-                    <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
-                        Start watching Laracasts
+                    <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quos ratione, totam minus pariatur tempore repellendus consectetur, unde vitae consequuntur quam alias rem, dolorem cupiditate consequatur sunt numquam ducimus et.
+                    </p>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500">
-                            <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                </p>
-            </div>
-
-            <div>
-                <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                    </svg>
-                    <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                        <a href="https://tailwindcss.com/">Tailwind</a>
-                    </h2>
+                    <p class="mt-4 text-sm">
+                        <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
+                            ENLACE????
+                        </a>
+                    </p>
                 </div>
 
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
-                </p>
-            </div>
+                <div>
+                    <div class="flex items-center">
+                        <RocketLaunchIcon class="stroke-cyan-200 h-16 w-16"></RocketLaunchIcon>
+                        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                            <a href="https://tailwindcss.com/">Vocaculario</a>
+                        </h2>
+                    </div>
 
+                    <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi accusamus recusandae excepturi? Earum suscipit libero nemo consequuntur totam sed, quisquam odit incidunt magni cupiditate aliquid, nisi unde. Itaque, nostrum maiores.
+                    </p>
+
+                    <p class="mt-4 text-sm">
+                        <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
+                            ENLACE????
+                        </a>
+                    </p>
+                </div>
+                <!--
             <div>
                 <div class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
@@ -99,6 +98,9 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
                     Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
                 </p>
             </div>
+             -->
+            </div>
+            
         </div>
     </div>
 </template>
