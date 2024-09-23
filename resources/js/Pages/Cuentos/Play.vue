8e5cf5a6b2fe1749ec81fe7d476bfd4c12e3ba91
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-09-23 03:21:37 -0600

    wip: ingaturroña esta más dificil de lo que pensé

--- resources/js/Pages/Cuentos/Play.vue
+++ resources/js/Pages/Cuentos/Play.vue
@@ -12,47 +12,130 @@ const props = defineProps({
 });
 
 let texto = ref('');
-
-let palabras = [];
+let palabras = ref<Array<HTMLElement | null>>([]);
 const $contenido = ref<HTMLElement | null>(null);
 const $input = ref<HTMLInputElement | null>(null);
 
 function iniciarJuego() {
     // Definimos las palabras del texto
-    palabras = props.cuento.contenido.split(' ').slice(0,32);
+    palabras.value = props.cuento.contenido.split(' ').slice(0, 32);
 
     // Mostramos las palabras en el DOM haciendo map para separar cada palabra y de
     // cada palabra, cada una de sus letras.
-    $contenido.value.innerHTML = palabras.map((palabra, index) => {
-        const letras = palabra.split('');
-
-        return `<word>
-            ${letras
-                .map(letra => `<letter>${letra}</letter>`)
-                .join('')
-            }
-        </word>
-        `
-    }).join('');
-
-    const $primera_palabra = $contenido.value.querySelector('word');
-    $primera_palabra.classList.add('font-semibold');
-    $primera_palabra.querySelector('letter').classList.add('bg-gray-200');
-
+    if ($contenido.value) {
+        $contenido.value.innerHTML = palabras.value.map((palabra) => {
+            const letras = palabra.split('');
+            return `<word>${letras.map(letra => `<letter>${letra}</letter>`).join('')}</word>`;
+        }).join('');
+    }
 }
 
 function iniciarEventos() {
+    // Fijamos el focus en el input al presionar cualquier tecla
     document.addEventListener('keydown', () => {
-        $input.value.focus();
+        $input.value?.focus();
     });
-    document.addEventListener('keyup', (e) => {
-        console.log(e.key);
 
-        if (e.key === $primera_palabra.querySelector('letter').textContent) {
-            texto.value = '';
+    const $palabra_actual = $contenido.value?.querySelector('word');
+    if ($palabra_actual) {
+        $palabra_actual.classList.add('current');
+    }
+    const $letra_actual = $palabra_actual?.querySelector('letter');
+    if ($letra_actual) {
+        $letra_actual.classList.add('current');
+    }
+
+    if ($input.value) {
+        $input.value.addEventListener('keydown', OnKeyDown);
+        $input.value.addEventListener('keyup', OnKeyUp);
+    }
+}
+
+function OnKeyDown(event) {
+    const $palabra_actual = $contenido.value?.querySelector('word.current');
+    const $letra_actual = $palabra_actual.querySelector('letter.current');
+
+    const { key } = event;
+    if (key === ' ') {
+        event.preventDefault();
+        const $nextPalabra = $palabra_actual?.nextElementSibling as HTMLElement;
+        if ($nextPalabra) {
+            $palabra_actual.classList.remove('current');
+            $nextPalabra.classList.add('current');
+
+            const $nextLetra = $nextPalabra.querySelector('letter');
+            if ($nextLetra) {
+                $letra_actual.classList.remove('current');
+                $nextLetra.classList.add('current');
+            }
+            $input.value!.value = '';
+        } else {
+            console.log("juego terminado");
+            // TODO GAMEOVER
+        }
+    }
+    if (key === 'Backspace') {
+        if ($input.value.value === '') {
+            event.preventDefault();
+            const $prevPalabra = $palabra_actual?.previousElementSibling as HTMLElement;
+            if ($prevPalabra) {
+                $palabra_actual.classList.remove('current');
+                $prevPalabra.classList.add('current');
+                $input.value.innerText = $prevPalabra.textContent?.trim() || '';
+
+                const $prevLetra = $prevPalabra.querySelector('letter');
+                if ($prevLetra) {
+                    $letra_actual.classList.remove('current');
+                    $prevLetra.classList.add('current');
+                }
+                $input.value!.value = '';
+            }
+        }
+    }
+}
+
+function OnKeyUp() {
+    const $palabra_actual = $contenido.value?.querySelector('word.current');
+    if ($palabra_actual) {
+        // Agregamos la clase current a la palabra actual
+        $palabra_actual.classList.add('current');
+
+        const $letra_actual = $palabra_actual.querySelector('letter.current');
+        if ($letra_actual) {
+            $letra_actual.classList.add('current');
         }
 
-    });
+        // Limitar la longitud del input al tamaño de la palabra actual
+        const palabra_actual = $palabra_actual.textContent?.trim() || '';
+        $input.value!.maxLength = palabra_actual.length;
+
+
+        // Limpiar clases anteriores
+        const todas_las_letras = $palabra_actual.querySelectorAll('letter');
+        todas_las_letras.forEach(letra => letra.classList.remove('correct', 'incorrect'));
+
+        // Comparar letras del input con la palabra actual
+        const inputValue = $input.value!.value.split('');
+        inputValue.forEach((letra, index) => {
+            const $letra = todas_las_letras[index];
+            if ($letra) {
+                $letra.classList.add('current');
+                const letra_a_comparar = palabra_actual[index];
+                const es_correcta = letra.toLowerCase() === letra_a_comparar.toLowerCase();
+                const letterClass = es_correcta ? 'correct' : 'incorrect';
+                $letra.classList.add(letterClass);
+            }
+        });
+        $letra_actual?.classList.remove('current', 'is-last');
+        const inputLength = inputValue.length;
+        const $nextActiveLetra = todas_las_letras[inputLength];
+        if ($nextActiveLetra) {
+            $nextActiveLetra.classList.add('current');
+        } else {
+            $letra_actual.classList.add('current', 'is-last');
+            // TODO GAMEOVER
+        }
+    }
 }
 
 onMounted(() => {
@@ -64,9 +147,9 @@ onMounted(() => {
 </script>
 
 <template>
-    <AppLayout :breadcrumbs="props.breadcrumbs">
+    <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex flex-col w-full text-center justify-center">
-            <h1 class="font-semibold text-2xl uppercase">{{props.cuento.titulo}}</h1>
+            <h1 class="font-semibold text-2xl uppercase">{{ cuento.titulo }}</h1>
             <span class="text-xs uppercase font-semibold">
                 Mejor score: {{ texto }}
             </span>
@@ -75,9 +158,47 @@ onMounted(() => {
 
         <div class="grid w-full h-full p-8 bg-gray-700 rounded-md shadow-md">
             <section id="juego">
-                <p id="contenido_juego" class="uppercase"></p>
+                <p id="contenido_juego" class="uppercase flex flex-wrap gap-1"></p>
                 <input v-model="texto" type="text" class="absolute top-0 left-0" autofocus>
             </section>
         </div>
     </AppLayout>
 </template>
+
+<style>
+
+letter {
+    position: relative;
+    /* &.current::before {
+        content: '|';
+        position: absolute;
+        left: -45%;
+        animation: 1s blink infinite ease-in-out;
+    }
+    &.current.is-last::before {
+        left: 45%;
+    } */
+}
+
+letter.correct {
+    color: white;
+}
+
+letter.incorrect {
+    color: red;
+}
+
+word.current {
+    text-decoration: underline;
+}
+
+/* @keyframes blink {
+        0%, 25% {
+            opacity: 1;
+        }
+        75% {
+            opacity: 0;
+        }
+    } */
+
+</style>
