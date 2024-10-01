9cc72d6eef2352a3ef95b4f0e2017500b6e1a06d
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-10-01 02:06:46 -0600

    UI: Eliminada imagen de banner, cambios en clases y agregadas animaciones de inicio

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -3,114 +3,83 @@ import { AcademicCapIcon, ChevronDoubleRightIcon, ChevronRightIcon, PaperAirplan
 </script>
 
 <template>
-    <div>
-
-        <div class="p-6 lg:p-8 bg-indigo-600 text-center flex items-center justify-center">
-
-            <div class="w-1/4 h-1/4">
-                <!-- Imagen temmporal-->
-                <img src="\images\typing.png" class="">
+    <transition appear name="left-to-right">
+        <div>
+            <div class="p-2 bg-indigo-600 text-center flex items-center justify-center">
+                <div class="m-8 columns-xl">
+                    <h1 class="mt-4 mb-2 text-6xl text-slate-50">
+                        Bienvenido a <h1 class="mb-4 mt-2 font-extrabold text-6xl text-slate-50">Érase una vez!</h1>
+                    </h1>
+                </div>
             </div>
-            
-            <div class="m-8 columns-xl">
-
-                <h1 class="mt-4 mb-2 text-7xl text-slate-50">
-                    Bienvenido a <h1 class="mb-4 mt-2 font-extrabold text-7xl text-slate-50">Érase una vez!</h1>
-                </h1>
-    
+            <!-- Espacio para listado de actividades-->
+            <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+                <!-- Lado izquierdo-->
+                <div class="bg-indigo-100/60 shadow-md transition-all duration-300 ease-in-out hover:bg-indigo-100 rounded-xl flex items-center">
+                    <img src="\images\typing.png">
+                    <!-- <a href="https://storyset.com/communication">Communication illustrations by Storyset</a> -->
+                </div>
+                <!-- Lado derecho-->
+                <!-- Contiene los espacios para los textos y sus "enlaces" correspondientes-->
+                <div class="space-y-3">
+                    <!-- Division para el espacio de Cursos-->
+                    <div class="bg-indigo-100/60 shadow-md transition-all duration-300 ease-in-out hover:bg-indigo-100 rounded-xl p-3">
+                        <div class="flex items-center">
+                            <AcademicCapIcon class="stroke-indigo-500 h-20 w-20"></AcademicCapIcon>
+                            <h2 class="ms-3 text-4xl font-semibold text-indigo-500">
+                                Cursos
+                            </h2>
+                        </div>
+                        <p class="mt-4 text-gray-900 text-lg leading-normal font-medium p-5 text-justify">
+                            Los tutores podrán asignar textos a sus alumnos para apoyar el desarrollo de las habilidades de
+                            los alumnos.
+                            Los alumnos podrán ingresar a cursos que les brindaran apoyo en el desarrollo de sus habilidades
+                            de escritura.
+                        </p>
+                        <button type="button"
+                            class="font-semibold mt-4 text-base inline-flex items-center text-indigo-500 hover:text-indigo-800">
+                            <PaperAirplaneIcon class="h-6 w-12 inline-flex items-center"></PaperAirplaneIcon>
+                            Explorar cursos disponibles
+                        </button>
+                    </div>
+                    <!-- Division para el espacio de cuentos -->
+                    <div class="bg-indigo-100/60 shadow-md transition-all duration-300 ease-in-out hover:bg-indigo-100 rounded-xl p-3">
+                        <div class="flex items-center">
+                            <PencilIcon class="stroke-indigo-500 h-20 w-20"></PencilIcon>
+                            <h2 class="ms-3 text-4xl font-semibold text-indigo-500">
+                                Cuentos
+                            </h2>
+                        </div>
+                        <p class="mt-4 text-gray-900 text-lg leading-normal font-medium p-5 text-justify">
+                            Pon a prueba tus habilidades de escritura con el modo "Cuentos" donde podrás encontrar una
+                            seleccián de historias con las cuales pondrás a volar la imaginación.
+                        </p>
+                        <button type="button"
+                            class="font-semibold mt-4 text-base inline-flex items-center text-indigo-500 hover:text-indigo-800">
+                            <PaperAirplaneIcon class="h-6 w-12 inline-flex items-center"></PaperAirplaneIcon>
+                            Explorar cuentos disponibles
+                        </button>
+                    </div>
+                    <!-- Division para el espacio de Modo Practica-->
+                    <div class="bg-indigo-100/60 shadow-md transition-all duration-300 ease-in-out hover:bg-indigo-100 rounded-xl p-3">
+                        <div class="flex items-center">
+                            <RocketLaunchIcon class="stroke-indigo-500 h-20 w-20"></RocketLaunchIcon>
+                            <h2 class="ms-3 text-4xl font-semibold text-indigo-500">
+                                Modo practica
+                            </h2>
+                        </div>
+                        <p class="mt-4 text-gray-900 text-lg font-medium leading-normal p-5 text-justify">
+                            Mejora tu habilidad y vocabulario dentro de este modo con una serie de palabras las cuales
+                            aumentaran su dificultad conforme aumente tu desempeño.
+                        </p>
+                        <button type="button"
+                            class="font-semibold mt-4 text-base inline-flex items-center text-indigo-500 hover:text-indigo-800">
+                            <PaperAirplaneIcon class="h-6 w-12 inline-flex items-center"></PaperAirplaneIcon>
+                            Comienza a practicar
+                        </button>
+                    </div>
+                </div>
             </div>
-            
         </div>
-
-        <!-- Espacio para listado de actividades-->
-        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-            
-            <!-- Lado izquierdo-->
-            <div class="bg-indigo-100 rounded-xl flex items-center">
-                <img src="\images\typing.png">
-                <!-- <a href="https://storyset.com/communication">Communication illustrations by Storyset</a> -->
-            </div>
-            
-            <!-- Lado derecho-->
-            <!-- Contiene los espacios para los textos y sus "enlaces" correspondientes-->
-            <div class="space-y-3">
-
-                <!-- Division para el espacio de Cursos-->
-                <div class="bg-indigo-100 rounded-xl p-3">
-
-                    <div class="flex items-center">
-
-                        <AcademicCapIcon class="stroke-indigo-500 h-20 w-20"></AcademicCapIcon>
-
-                        <h2 class="ms-3 text-4xl font-semibold text-indigo-500">
-                            Cursos
-                        </h2>
-
-                    </div>
-
-                    <p class="mt-4 text-gray-900 text-lg leading-normal font-medium p-5">
-                        Los tutores podrán asignar textos a sus alumnos para apoyar el desarrollo de las habilidades de los alumnos.
-                        Los alumnos podrán ingresar a cursos que les brindaran apoyo en el desarrollo de sus habilidades de escritura.
-                    </p>
-
-                    <button type="button" class="font-semibold mt-4 text-base inline-flex items-center text-indigo-500 hover:text-indigo-800">
-                        <PaperAirplaneIcon class="h-6 w-12 inline-flex items-center"></PaperAirplaneIcon>
-                        Explorar cursos disponibles
-                    </button>
-                
-                </div>
-
-                <!-- Division para el espacio de cuentos -->
-                <div class="bg-indigo-100 rounded-xl p-3">
-
-                    <div class="flex items-center">
-
-                        <PencilIcon class="stroke-indigo-500 h-20 w-20"></PencilIcon>
-
-                        <h2 class="ms-3 text-4xl font-semibold text-indigo-500">
-                            Cuentos
-                        </h2>
-
-                    </div>
-
-                    <p class="mt-4 text-gray-900 text-lg leading-normal font-medium p-5">
-                        Pon a prueba tus habilidades de escritura con el modo "Cuentos" donde podrás encontrar una seleccián de historias con las cuales pondrás a volar la imaginación.
-                    </p>
-
-                    <button type="button" class="font-semibold mt-4 text-base inline-flex items-center text-indigo-500 hover:text-indigo-800">
-                        <PaperAirplaneIcon class="h-6 w-12 inline-flex items-center"></PaperAirplaneIcon>
-                        Explorar cuentos disponibles
-                    </button>
-
-                </div>
-
-                <!-- Division para el espacio de Modo Practica-->
-                <div class="bg-indigo-100 rounded-xl p-3">
-
-                    <div class="flex items-center">
-
-                        <RocketLaunchIcon class="stroke-indigo-500 h-20 w-20"></RocketLaunchIcon>
-
-                        <h2 class="ms-3 text-4xl font-semibold text-indigo-500">
-                            Modo practica
-                        </h2>
-
-                    </div>
-
-                    <p class="mt-4 text-gray-900 text-lg font-medium leading-normal p-5">
-                        Mejora tu habilidad y vocabulario dentro de este modo con una serie de palabras las cuales aumentaran su dificultad conforme aumente tu desempeño.
-                    </p>
-
-                    <button type="button" class="font-semibold mt-4 text-base inline-flex items-center text-indigo-500 hover:text-indigo-800">
-                        <PaperAirplaneIcon class="h-6 w-12 inline-flex items-center"></PaperAirplaneIcon>
-                        Comienza a practicar
-                    </button>
-
-                </div>
-            
-            </div>
-            
-        </div>
-        
-    </div>
+    </transition>
 </template>
