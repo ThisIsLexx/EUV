2d91a0691af04ad976b083ec3d9cb9aadbf22187
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-10-01 02:06:19 -0600

    UI: Agregadas animaciones de inicio, botones para acceder a secciones y transiciones.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -16,11 +16,19 @@ function handleImageError() {
     document.getElementById('docs-card-content')?.classList.add('!flex-row');
     document.getElementById('background')?.classList.add('!hidden');
 }
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
 </script>
 
 <template>
 
-    <Head title="Welcome" />
+    <Head title="Bienvenida" />
     <div class="relative w-full">
         <nav class="fixed z-10 w-full bg-white md:absolute md:bg-transparent">
             <div class="container m-auto px-2 md:px-12 lg:px-7">
@@ -45,7 +53,11 @@ function handleImageError() {
                         class="hidden w-full lg:flex flex-wrap justify-end items-center space-y-6 p-6 rounded-xl bg-white md:space-y-0 md:p-0 md:flex-nowrap md:bg-transparent lg:w-7/12">
 
                         <div class="w-full space-y-2 lg:space-y-0 md:w-max">
-                            <span class="block text-yellow-800 font-semibold text-sm">
+                            <span class="flex space-x-2 items-center text-yellow-800 font-semibold text-sm">
+                                <Link
+                                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-m text-indigo-500 uppercase tracking-widest hover:bg-indigo-200 focus:bg-indigo-200 active:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+                                Registrarme
+                                </Link>
                                 <Link :href="route('login')"
                                     class="inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold text-m text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
                                 ACCEDER
@@ -61,81 +73,113 @@ function handleImageError() {
                 <div class="flex items-center flex-wrap px-2 md:px-0">
                     <div class="relative lg:w-6/12 lg:py-24 xl:py-32">
                         <h1 class="font-bold text-4xl text-gray-800 md:text-5xl lg:w-10/12">Tu aventura en la
-                            <span class="text-indigo-400">mecanografía</span> comienza aquí.</h1>    
-                            
+                            <span class="text-indigo-400">mecanografía</span> comienza aquí.
+                        </h1>
+
                         <div class="pt-12 flex space-x-4">
-                            <Link :href="route('login')"
-                                class="inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold text-m text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+                            <transition appear name="slide-fade">
+                                <Link :href="route('login')"
+                                    class="inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold text-m text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-300">
                                 Iniciar
-                            </Link>
-                            
-                            <button :type="type" class="inline-flex items-center px-4 py-2 bg-white-800 border-indigo-800 border-2 border-transparent rounded-md font-semibold text-m text-indigo-800 uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 focus:text-white hover:text-white active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
-                                Ver Más
-                            </button>
+                                </Link>
+                            </transition>
+                            <transition appear name="slide-fade">
+                                <a @click="scrollToSection('acerca')"
+                                    class="inline-flex items-center px-4 py-2 bg-white-800 border-indigo-800 border-2 border-transparent rounded-md font-semibold text-m text-indigo-800 uppercase tracking-widest hover:bg-indigo-400 focus:bg-indigo-700 focus:text-white hover:text-white active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-300 hover:cursor-pointer">
+                                    Ver Más
+                                </a>
+                            </transition>
                         </div>
                     </div>
 
+                    <transition appear name="slide-fade" :style="{ transitionDelay: '200ms' }">
+                        <div class="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
+                            <img src="/images/landing/e-learning.png" class="relative" alt="laptop" loading="lazy"
+                                width="4500" height="4500">
+                        </div>
+                    </transition>
 
-                    <div class="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
-                        <img src="/images/landing/e-learning.png" class="relative" alt="laptop" loading="lazy"
-                            width="4500" height="4500">
-                    </div>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- component -->
-    <div class="mt-36 py-16 bg-white">
+    <div class="mt-36 py-16 bg-white shadow-2xl shadow-black z-10" id="acerca">
         <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
             <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                 <div class="md:5/12 lg:w-5/12">
-                    <img src="/images/landing/laptop.png" alt="image"
-                        loading="lazy" width="" height="">
+                    <img src="/images/landing/laptop.png" alt="image" loading="lazy" width="" height="">
                 </div>
-                <div class="md:7/12 lg:w-6/12">
+                <div class="md:7/12 lg:w-6/12 text-justify">
                     <h2 class="text-2xl text-indigo-700 font-bold md:text-4xl">Bienvenidos a Érase Una Vez</h2>
-                    <p class="mt-6 text-gray-600">En Érase Una Vez, creemos que la lectura y la mecanografía son habilidades fundamentales que enriquecen la vida de los estudiantes. Nuestra plataforma está diseñada para ofrecer una experiencia única que combina la mejora del uso del teclado con el fomento de la lectura y la expansión del vocabulario.</p>
-                    <p class="mt-4 text-gray-600">Aquí, los tutores y maestros pueden crear textos adaptados a diferentes niveles de dificultad, permitiendo a los alumnos avanzar a su propio ritmo. Nos apasiona ayudar a los educadores a enriquecer sus clases y a los estudiantes a descubrir el placer de escribir y leer. Con Érase Una Vez, cada palabra cuenta y cada lección es una aventura.</p>
+                    <p class="mt-6 text-gray-600">En Érase Una Vez, creemos que la lectura y la mecanografía son
+                        habilidades fundamentales que enriquecen la vida de los estudiantes. Nuestra plataforma está
+                        diseñada para ofrecer una experiencia única que combina la mejora del uso del teclado con el
+                        fomento de la lectura y la expansión del vocabulario.</p>
+                    <p class="mt-4 text-gray-600">Aquí, los tutores y maestros pueden crear textos adaptados a
+                        diferentes niveles de dificultad, permitiendo a los alumnos avanzar a su propio ritmo. Nos
+                        apasiona ayudar a los educadores a enriquecer sus clases y a los estudiantes a descubrir el
+                        placer de escribir y leer. Con Érase Una Vez, cada palabra cuenta y cada lección es una
+                        aventura.</p>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- component -->
-    <div class="py-16 bg-indigo-500">
+    <div class="py-16 bg-indigo-500" id="servicios">
         <div class="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
             <div class="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
-                <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                    <div class="mb-12 space-y-4">
-                        <h3 class="text-2xl font-semibold text-indigo-900">Creación de Textos Personalizados</h3>
-                        <p class="mb-6">Los maestros pueden diseñar textos en tres niveles de dificultad: baja, media y difícil. Esto permite que cada estudiante trabaje en su nivel, asegurando un aprendizaje efectivo y motivador.</p>
+                <transition appear name="slide-fade">
+                    <div
+                        class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 transition-all duration-300 ease-in-out hover:scale-105">
+                        <div class="mb-12 space-y-4">
+                            <h3 class="text-2xl font-semibold text-indigo-900">Creación de Textos Personalizados</h3>
+                            <p class="mb-6 text-justify">Los maestros pueden diseñar textos en tres niveles de
+                                dificultad: baja, media y difícil. Esto permite que cada estudiante trabaje en su nivel,
+                                asegurando un aprendizaje efectivo y motivador.</p>
+                        </div>
+                        <img src="https://tailus.io/sources/blocks/end-image/preview/images/graphic.svg"
+                            class="w-2/3 ml-auto mr-24 -mb-12" alt="illustration" loading="lazy" width="900"
+                            height="600">
                     </div>
-                    <img src="https://tailus.io/sources/blocks/end-image/preview/images/graphic.svg"
-                        class="w-2/3 ml-auto mr-24 -mb-12" alt="illustration" loading="lazy" width="900" height="600">
-                </div>
-                <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                    <div class="mb-12 space-y-4">
-                        <h3 class="text-2xl font-semibold text-indigo-900">Cursos Interactivos</h3>
-                        <p class="mb-6">Los alumnos pueden inscribirse en cursos impartidos por sus tutores, quienes podrán asignarles textos específicos. Esta estructura permite un seguimiento del progreso y una atención personalizada.</p>
+                </transition>
+                <transition appear name="slide-fade">
+                    <div
+                        class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 transition-all duration-300 ease-in-out hover:scale-105">
+                        <div class="mb-12 space-y-4">
+                            <h3 class="text-2xl font-semibold text-indigo-900">Cursos Interactivos</h3>
+                            <p class="mb-6 text-justify">Los alumnos pueden inscribirse en cursos impartidos por sus
+                                tutores, quienes podrán asignarles textos específicos. Esta estructura permite un
+                                seguimiento del progreso y una atención personalizada.</p>
+                        </div>
+                        <img src="/images/landing/purple-writer.png" class="w-2/3 ml-auto mr-16" alt="illustration"
+                            loading="lazy" width="900" height="600">
                     </div>
-                    <img src="/images/landing/purple-writer.png"
-                        class="w-2/3 ml-auto mr-16" alt="illustration" loading="lazy" width="900" height="600">
-                </div>
-                <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                    <div class="mb-12 space-y-4">
-                        <h3 class="text-2xl font-semibold text-indigo-900">Mejora de la Mecanografía</h3>
-                        <p class="mb-6">A través de ejercicios diseñados para practicar la escritura, los usuarios pueden mejorar su velocidad y precisión en el teclado, habilidades que son esenciales en la era digital.</p>
+                </transition>
+                <transition appear name="slide-fade">
+                    <div
+                        class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 transition-all duration-300 ease-in-out hover:scale-105">
+                        <div class="mb-12 space-y-4">
+                            <h3 class="text-2xl font-semibold text-indigo-900">Mejora de la Mecanografía</h3>
+                            <p class="mb-6 text-justify">A través de ejercicios diseñados para practicar la escritura,
+                                los usuarios pueden mejorar su velocidad y precisión en el teclado, habilidades que son
+                                esenciales en la era digital.</p>
+                        </div>
+                        <img src="/images/landing/writing.png" class="w-2/3 ml-auto mr-16" alt="illustration"
+                            loading="lazy" width="900" height="600">
                     </div>
-                    <img src="/images/landing/writing.png"
-                        class="w-2/3 ml-auto mr-16" alt="illustration" loading="lazy" width="900" height="600">
-                </div>
+                </transition>
+
+
             </div>
         </div>
     </div>
 
     <!-- component -->
     <footer>
+
         <body class="bg-white">
             <div class="flex flex-col mx-3 bg-white rounded-lg">
                 <div class="w-full draggable">
@@ -144,12 +188,14 @@ function handleImageError() {
                             <div class="flex flex-col items-center gap-6 mb-8">
                                 <div
                                     class="flex flex-wrap items-center justify-center gap-5 lg:gap-12 gap-y-3 lg:flex-nowrap text-dark-grey-900">
-                                    <a href="javascript:void(0)" class="text-gray-600 hover:text-gray-900">Acerca de</a>
-                                    <a href="javascript:void(0)" class="text-gray-600 hover:text-gray-900">Características</a>
-                                    <Link :href="route('login')" class="text-gray-600 hover:text-gray-900">Acceder</Link>
+                                    <a @click="scrollToSection('acerca')" class="text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600 hover:cursor-pointer">Acerca de</a>
+                                    <a @click="scrollToSection('servicios')" class="text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600 hover:cursor-pointer">Servicios</a>
+                                    <Link :href="route('login')" class="text-gray-600 hover:text-gray-900">Acceder
+                                    </Link>
                                 </div>
                                 <div class="flex items-center gap-8">
-                                    <a href="https://github.com/ThisIsLexx/EUV" class="text-grey-700 hover:text-grey-900">
+                                    <a href="https://github.com/ThisIsLexx/EUV"
+                                        class="text-grey-700 hover:text-grey-900">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none">
                                             <path
@@ -161,7 +207,8 @@ function handleImageError() {
                             </div>
                             <div class="flex items-center">
                                 <p class="text-base font-normal leading-7 text-center text-grey-700">
-                                    Universidad de Guadalajara | Centro Universitario de Ciencias Exactas e Ingenierías</p>
+                                    Universidad de Guadalajara | Centro Universitario de Ciencias Exactas e Ingenierías
+                                </p>
                             </div>
                         </div>
                     </div>
