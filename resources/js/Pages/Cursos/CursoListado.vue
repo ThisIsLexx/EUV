19a4c116512dece09315fb2df0589f9f6941a2b4
Higashi <seigi.higashi@gmail.com>
2024-10-05 03:59:16 -0600

    Update CursoListado.vue
    Comienzo de la muestra del listado de cursos, estoy aprendiendo a mostrar la información recuperada

--- resources/js/Pages/Cursos/CursoListado.vue
+++ resources/js/Pages/Cursos/CursoListado.vue
@@ -3,12 +3,91 @@ import { ref } from 'vue';
 import { Curso } from '@/types/curso';
 import AppLayout from '@/Layouts/AppLayout.vue';
 
+/* Variable con los cursos */
+const props = defineProps({
+    curso: {
+        type: Object as () => Curso,
+        required: true,
+    }
+});
+
+/*Funcion para colores de cursos*/
+function resolveCursoColor(color: string) {
+    switch (color) {
+        case 'blue':
+            return 'bg-gradient-to-r from-sky-500 to-indigo-500';
+        case 'emerald':
+            return 'bg-gradient-to-r from-cyan-500 to-emerald-500';
+        case 'red':
+            return 'bg-gradient-to-r from-rose-500 to-red-500';
+        case 'amber':
+            return 'bg-gradient-to-r from-amber-500 to-yellow-500';
+        case 'orange':
+            return 'bg-gradient-to-r from-orange-500 to-yellow-500';
+        case 'purple':
+            return 'bg-gradient-to-r from-violet-500 to-purple-500';
+        case 'gray':
+            return 'bg-gradient-to-r from-gray-500 to-gray-800';
+        case 'pink':
+            return 'bg-gradient-to-r from-pink-500 to-rose-500';
+        default:
+            return 'bg-gradient-to-r from-cyan-500 to-emerald-500';
+    }
+}
+
 </script>
 
 <template>
+
     <AppLayout>
+
         <div>
-            Aqui listado de cursos
+
+            <!-- Contenedor principal-->
+            <div class="bg-indigo-200 bg-opacity-25 rounded-xl grid grid-cols-3 items-center p-6">
+                
+                <!-- Caso de lista vacia -->
+                <div>
+                    <h1>VACIO</h1>
+                </div>
+
+                <!-- Caso de lista con elementos -->
+                <div>
+
+                    <!-- Columna para imagen-->
+                    <div class="w-1/2 h-1/2 aspect-square items-center">
+                        <img src="\images\typing.png" class="border border-black rounded-lg">
+                    </div>
+
+                    <!-- Columna para titulo y descripcion-->
+                    <div>
+
+                        <h1>
+
+                            Titulo
+
+                        </h1>
+
+                        <h1>
+
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem maiores, sit ut facere earum ab et quas atque aspernatur, possimus tenetur. Odit omnis delectus voluptatibus numquam officiis exercitationem doloremque! Necessitatibus?
+
+                        </h1>
+
+                    </div>
+                    <!-- Columna para codigo-->
+                    <h3>
+
+                        codigo
+
+                    </h3>
+                    
+                </div>
+
+            </div>
+            <!-- Fin de contenedor-->
         </div>
+
     </AppLayout>
+
 </template>
