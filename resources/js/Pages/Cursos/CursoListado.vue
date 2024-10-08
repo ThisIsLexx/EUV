4c0ee7e377aaedee89eafe28a9dc8e8aefcdb055
Higashi <seigi.higashi@gmail.com>
2024-10-08 01:14:01 -0600

    Update CursoListado.vue
    Modifique el acomodo de la imagen para que no tenga espacios entre ella y el borde del contenedor

--- resources/js/Pages/Cursos/CursoListado.vue
+++ resources/js/Pages/Cursos/CursoListado.vue
@@ -46,10 +46,10 @@ function getNameTutor(identificador : number){
                 <!-- Caso de lista con elementos -->
                 <div v-else>
                     <div v-for="curso in props.cursos" :key="curso.id">
-                        <div class="w-full rounded-xl grid grid-cols-3 items-center p-6 mt-3 mb-3 bg-gray-200">
+                        <div class="w-full rounded-xl grid grid-cols-3 items-center mt-4 mb-4 bg-gray-200">
                             <!-- Columna para imagen-->
-                            <div class="flex justify-center mr-6">
-                                <img :src="'/images/curso_images/' + curso.color + '.jpg'" class="rounded-lg h-3/4 w-3/4 object-cover rounded-t-md transition-all duration-300 transform brightness-50 group-hover:brightness-100">
+                            <div class="flex justify-start h-full">
+                                <img :src="'/images/curso_images/' + curso.color + '.jpg'" class="rounded-lg h-full w-full object-cover rounded-t-md transition-all duration-300 transform brightness-50 group-hover:brightness-100">
                             </div>
                             <!-- Columna para titulo y descripcion-->
                             <div class="p-6">
