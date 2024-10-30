5241cc4d2f36f4e4e70a031e95d73a881318481e
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2024-10-30 02:16:38 -0600

    feat: ahora si jala el modo práctica...

--- resources/js/Pages/Practice.vue
+++ resources/js/Pages/Practice.vue
@@ -1,45 +1,82 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
 import Separator from '@/Components/Separator.vue';
+import Modal from '@/Components/Custom/Modal.vue';
+import baja from '@/Components/Custom/baja.json';
+import media from '@/Components/Custom/media.json';
+import alta from '@/Components/Custom/alta.json';
+import { ArrowPathRoundedSquareIcon } from '@heroicons/vue/24/outline';
 
-const texto = ref('Hola, soy un ejemplo de texto');
+
+const palabrasDificultadBaja = baja['palabras'];
+const palabrasDificultadMedia = media['palabras'];
+const palabrasDificultadAlta = alta['palabras'];
+
 const $input = ref<HTMLInputElement | null>(null);
 const palabras = ref<Array<string>>([]);
+const texto = ref<string>('hola mundo');
 
 const palabrasUsuario = ref<Array<string>>([]);
 
-const tiempo_transcurrido = ref(0);
+const tiempo_transcurrido = ref<number>(0);
 
-const palabraActualIndex = ref(0); // Indice de la palabra actual
-const letraActualIndex = ref(0); // Indice de la letra actual
+const palabraActualIndex = ref<number>(0); // Indice de la palabra actual
+const letraActualIndex = ref<number>(0); // Indice de la letra actual
 
-const input_usuario = ref('');
+const input_usuario = ref<string>('');
+const showModal = ref<boolean>(false);
+const PPM = ref<number>(0);
 
 onMounted(() => {
     iniciarJuego();
-    startTimer();
-    $input.value?.focus();
+
 });
 
-let timer: any | undefined;
+const timer = ref<number | null>(null);
 
 function startTimer() {
-    timer = setInterval(() => {
+    stopTimer(); // Ensure any existing timer is stopped before starting a new one
+    timer.value = window.setInterval(() => {
         tiempo_transcurrido.value++;
     }, 1000);
 }
 
 function stopTimer() {
-    if (timer) {
-        clearInterval(timer);
-        timer = undefined;
+    if (timer.value !== null) {
+        clearInterval(timer.value);
+        timer.value = null;
     }
 }
 
+const props = defineProps<{
+    dificultad: string;
+}>();
+
 function iniciarJuego() {
-    palabras.value = texto.value.split(' ');
+    startTimer();
+    $input.value?.focus();
+    if (props.dificultad === 'baja') {
+        palabras.value = shuffleArray(palabrasDificultadBaja).slice(0, 30);
+    }
+    else if (props.dificultad === 'media') {
+        palabras.value = shuffleArray(palabrasDificultadMedia).slice(0, 30);
+    }
+    else if (props.dificultad === 'alta') {
+        palabras.value = shuffleArray(palabrasDificultadAlta).slice(0, 30);
+    }
+    else {
+        palabras.value = shuffleArray(palabrasDificultadBaja).slice(0, 30);
+    }
+}
+
+function shuffleArray(array: Array<string>): Array<string> {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 function onKeyDown(event: KeyboardEvent) {
@@ -55,31 +92,31 @@ function onKeyDown(event: KeyboardEvent) {
 }
 
 function onKeyUp(event: KeyboardEvent) {
+    console.log('letraActualIndex', letraActualIndex.value);
+    // Casos a ignorar / espacios y acentos
+    if (event.key === ' ') return;
+    if (event.key === 'Dead') return;
 
-    if (event.key === ' ') {
-        return;
-    }
-
+    // Lógica para implementación de tecla para borrar
     if (event.key === 'Backspace') {
         if (letraActualIndex.value > 0) {
             letraActualIndex.value--;
-        }
-        if (letraActualIndex.value === 0) {
-            if (palabraActualIndex.value > 0) {
-                palabraActualIndex.value--;
-                letraActualIndex.value = palabras.value[palabraActualIndex.value].length;
-            }
-            letraActualIndex.value = palabras.value[palabraActualIndex.value].length;
+        } else if (palabraActualIndex.value > 0) {
+            palabraActualIndex.value--;
+            letraActualIndex.value = palabras.value[palabraActualIndex.value].length - 1;
+            input_usuario.value = palabras.value[palabraActualIndex.value];
+            palabrasUsuario.value.pop();
         }
         return;
     }
 
     const palabraActual = palabras.value[palabraActualIndex.value];
-    const letraIndex = letraActualIndex.value;
-
     letraActualIndex.value++;
 
     // Si se llega al final de la palabra actual
+
+    if (!palabraActual) gameOver();
+
     if (letraActualIndex.value >= palabraActual.length) {
         palabraActualIndex.value++;
         letraActualIndex.value = 0;
@@ -88,11 +125,7 @@ function onKeyUp(event: KeyboardEvent) {
     }
 
     if (palabraActualIndex.value >= palabras.value.length) {
-        console.log('Juego terminado');
-        console.log('Palabras correctas', totalPalabrasCorrectas());
-        stopTimer();
-        console.log('Tiempo total trasncurrido', tiempo_transcurrido?.value);
-        console.log('PPM', totalPalabrasCorrectas() / (tiempo_transcurrido?.value / 60));
+        gameOver();
     }
 }
 
@@ -104,11 +137,40 @@ function esLetraCorrecta(palabraIndex: number, letraIndex: number): boolean {
 }
 
 function esPalabraCorrecta(palabraIndex: number): boolean {
-    return palabrasUsuario.value[palabraIndex] === palabras.value[palabraIndex];
+    return palabrasUsuario.value[palabraIndex]?.toLowerCase() === palabras.value[palabraIndex]?.toLowerCase();
 }
 
-function totalPalabrasCorrectas(): number {
-    return document.querySelectorAll('.word.correct').length;
+const totalPalabrasCorrectas = computed(() => {
+    return palabrasUsuario.value.filter((palabra, index) => esPalabraCorrecta(index)).length;
+});
+
+function gameOver(): void {
+    try {
+        stopTimer();
+        PPM.value = (totalPalabrasCorrectas.value * 60) / tiempo_transcurrido?.value;
+    }
+    finally {
+        showModal.value = true;
+    }
+
+}
+
+function reinitGame() {
+    stopTimer(); // Stop the existing timer
+    palabrasUsuario.value = [];
+    palabraActualIndex.value = 0;
+    letraActualIndex.value = 0;
+    tiempo_transcurrido.value = 0;
+    iniciarJuego();
+    startTimer();
+    $input.value?.focus();
+    showModal.value = false;
+}
+
+function endGame() {
+    showModal.value = false;
+    $input.value?.blur();
+    router.get('/estadisticas');
 }
 
 </script>
@@ -116,43 +178,81 @@ function totalPalabrasCorrectas(): number {
 <template @keydown="$input.value?.focus()">
     <Head title="Modo Práctica" />
     <AppLayout>
+        <Modal :showModal="showModal" :closeButton="false" size="lg" titulo="Resultados del modo práctica" @closeModal="showModal = false;">
+            <template v-slot:modal-content>
+
+                <div class="flex flex-col w-full text-center justify-center">
+                    <h1 class="font-semibold text-3xl uppercase">¡Felicidades!</h1>
+                    <span class="text-base uppercase mt-2">
+                        Has finalizado el <span class="text-indigo-500 font-bold"> modo práctica</span>
+                    </span>
+                    <span class="uppercase text-lg mt-2">
+                        Estadísticas obtenidas
+                    </span>
+                    <hr>
+
+                    <div class="flex w-full justify-between gap-2">
+                        <span class="uppercase w-full text-justify mt-2">
+                            Palabras totales: <span class="text-indigo-500 font-semibold">{{ palabras.length }}</span>
+                        </span>
+                        <span class="uppercase w-full text-justify mt-2">
+                            Palabras correctas: <span class="text-indigo-500 font-semibold">{{ totalPalabrasCorrectas }}</span>
+                        </span>
+                    </div>
+                    <div class="flex w-full justify-between gap-2">
+                        <span class="uppercase w-full text-justify">
+                            Tiempo transcurrido: <span class="text-indigo-500 font-semibold">{{ tiempo_transcurrido }} segundos</span>
+                        </span>
+                        <span class="uppercase w-full text-justify">
+                            Palabras por minuto: <span class="text-indigo-500 font-semibold">{{ PPM.toFixed(2) }}</span>
+                        </span>
+                    </div>
+
+                </div>
+            </template>
+            <template v-slot:action-button>
+                <button type="button" class="bg-gray-500 hover:bg-gray-500/90 text-white shadow-sm rounded-md p-2"
+                    @click="endGame">
+                    Salir del modo práctica
+                </button>
+                <button type="button" class="bg-indigo-500 hover:bg-indigo-500/90 text-white shadow-sm rounded-md p-2"
+                    @click="reinitGame">
+                    Volver a jugar
+                </button>
+            </template>
+        </Modal>
         <div class="flex flex-col px-20 w-full">
             <div class="flex flex-col w-full text-center justify-center">
                 <h1 class="font-semibold text-2xl uppercase text-indigo-700 underline underline-offset-4">Modo práctica</h1>
-                <span class="text-base uppercase font-semibold">
+                <span class="text-base uppercase font-semibold mt-4">
                     Tiempo transcurrido: <span class="text-indigo-500">{{ tiempo_transcurrido }}</span>
-                </span>
-                <span class="text-base uppercase font-semibold mt-4">
-                    Palabra actual: <span class="text-indigo-500">{{ palabras[palabraActualIndex] }} </span>
-                </span>
-                <span class="text-base uppercase font-semibold mt-4">
-                    Input:
-                    {{ $input?.value }}
                 </span>
             </div>
             <Separator/>
             <div class="grid w-full h-full p-8 bg-gray-700 text-gray-400 rounded-md shadow-md">
                 <section id="juego">
                     <p id="contenido_juego" class="uppercase text-6xl transition-colors duration-100 ease-in-out flex flex-wrap w-full items-center justify-center gap-2">
-                        <span v-for="(palabra, palabraIndex) in palabras" v-show="palabraIndex === palabraActualIndex"
-                              :key="palabraIndex"
-                              class="word"
-                              :class="{
-                                  'current': palabraIndex === palabraActualIndex,
-                                  'correct': esPalabraCorrecta(palabraIndex),
-                                  'incorrect': !esPalabraCorrecta(palabraIndex) && palabraIndex < palabraActualIndex
-                              }">
-                            <span v-for="(letra, letraIndex) in palabra.split('')"
-                                  :key="letraIndex"
-                                  class="letter"
+                        <transition-group name="slide">
+                            <span v-for="(palabra, palabraIndex) in palabras" v-show="palabraIndex === palabraActualIndex"
+                                  :key="palabraIndex"
+                                  class="word"
                                   :class="{
-                                      'current': letraIndex === letraActualIndex && palabraIndex === palabraActualIndex,
-                                      'correct': esLetraCorrecta(palabraIndex, letraIndex) && palabraIndex === palabraActualIndex,
-                                      'incorrect': !esLetraCorrecta(palabraIndex, letraIndex) && palabraIndex === palabraActualIndex && letraIndex < letraActualIndex
+                                      'current': palabraIndex === palabraActualIndex,
+                                      'correct': esPalabraCorrecta(palabraIndex),
+                                      'incorrect': !esPalabraCorrecta(palabraIndex) && palabraIndex < palabraActualIndex
                                   }">
-                                {{ letra }}
+                                <span v-for="(letra, letraIndex) in palabra.split('')"
+                                      :key="letraIndex"
+                                      class="letter"
+                                      :class="{
+                                          'current': letraIndex === letraActualIndex && palabraIndex === palabraActualIndex,
+                                          'correct': esLetraCorrecta(palabraIndex, letraIndex) && palabraIndex === palabraActualIndex,
+                                          'incorrect': !esLetraCorrecta(palabraIndex, letraIndex) && palabraIndex === palabraActualIndex && letraIndex < letraActualIndex
+                                      }">
+                                    {{ letra }}
+                                </span>
                             </span>
-                        </span>
+                        </transition-group>
                     </p>
                     <input v-model="input_usuario"
                            @keydown="onKeyDown"
@@ -216,4 +316,14 @@ span.word.incorrect {
         opacity: 0;
     }
 }
+
+.slide-enter-active {
+    transition: all 0.5s ease;
+}
+
+.slide-enter-from {
+    opacity: 0;
+    transform: translateY(30px);
+}
+
 </style>
